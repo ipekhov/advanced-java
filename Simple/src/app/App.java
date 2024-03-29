d1b02de5953f16d89ffd4c041b0035953c2a10ae
Ivan P. <ivanpekhov@gmail.com>
2024-03-29 13:27:10 -0400

    Changed to Hello World 2

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -7,6 +7,6 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
     }
 }
